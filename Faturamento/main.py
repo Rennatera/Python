adfcf2643c94acf8b90cc3994a9b0c52f2273f59
rennatera <renato.soc@gmail.com>
2022-06-08 22:45:53 -0300

    new

---
+++ Faturamento/main.py
@@ -0,0 +1,7 @@
+# importar a base de dados
+# visualizar a base de dados
+# Faturamento por loja
+# quantidade de produtos vendidos por loja
+# ticket médio por produto em casa loja
+# envia email com o relatório
+
